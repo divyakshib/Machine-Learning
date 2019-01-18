0718f9ecca8f3cd48cc222448dd1e121d444c150
divyakshib <divyakshi.bhardwaj@gmail.com>
2019-01-19 00:43:05 +0530

    Logistic Regression.py

--- Logistic Regression.py
+++ Logistic Regression.py
@@ -17,4 +17,36 @@ x=np.vstack((x1,x2))
         plt.scatter(x[i,0],x[i,1],color='green')'''
 plt.scatter(x[:,0],x[:,1],c=y)
 plt.show()
-def hypothsis():
+def hypothsis(x,w,b):
+    h=np.dot(x,w)+b
+    return sigmoid(h)
+def sigmoid(z):
+    return 1.0/(1.0+np.exp(-1.0*z))
+def error(y_,x,w,b):
+    m=x.shape[0]
+    er=0
+    for i in range(m):
+        hx=hypothsis(x[i],w,b)
+        er+=y_[i]*np.log2(hx)+(1-y)*np.log2(1-hx)
+    return -er/m
+def grd(y_,x,w,b):
+    grad_w=np.zeros(w.shape)
+    grad=0.0
+    m=x.shape[0]
+    for i in range(m):
+        hx=hypothsis(x[i],w,b)
+        grad_x=-1*(y_[i]-hx)*x[i]
+        grad=-1*(y_[i]-hx)
+    grad_w/=m
+    grad/=m
+    return [grad_w,grad]
+def gradient_descent(x,y_,w,b,l=0):
+    err=error(y_,x,w,b)
+    [gw,gb]=grd(y_,x,w,b)
+    w=w+l*gw
+    b=b+l*gb
+    return err,w,b
+W=y**2
+b=y**3
+err,w,b=gradient_descent(x[:,0],x[:,1],W,b)
+print(err)
